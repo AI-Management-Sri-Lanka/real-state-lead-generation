1d68e8f5342b1d660556b3ce3a97c1a9e380f0e8
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2026-05-20 20:20:43 +0530

    Delete src directory

--- src/hooks/useAuth.ts
+++
@@ -1,9 +0,0 @@
-// src/hooks/useAuth.ts
-import { useContext } from 'react'
-import { AuthContext, AuthContextType } from '@/contexts/AuthContext'
-
-export function useAuth(): AuthContextType {
-  const ctx = useContext(AuthContext)
-  if (!ctx) throw new Error('useAuth must be used inside <AuthProvider>')
-  return ctx
-}
