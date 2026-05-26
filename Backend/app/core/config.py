872d6c9267a3f4195949f948af5a1d99e7f3f83a
Asindu De Silva <129645521+AsinduDeSilva@users.noreply.github.com>
2026-05-26 21:13:11 +0530

    feat: add QDRANT_URL to settings

--- Backend/app/core/config.py
+++ Backend/app/core/config.py
@@ -1,10 +1,14 @@
 from pydantic_settings import BaseSettings
 
+from app.core.params import BASE_DIR
+
+
 class Settings(BaseSettings):
     DATABASE_URL: str
-    
+    QDRANT_URL: str
+
     class Config:
-        env_file = ".env"
+        env_file = BASE_DIR / ".env"
         extra = "ignore"
 
 settings = Settings()
