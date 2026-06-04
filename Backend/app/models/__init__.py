8d4422ad3d3a56b8b1a18eff6f6592a67da9a767
Pasindu Wickramasinghe <dulanjaya.pasindu1130@gmail.com>
2026-06-04 06:34:03 +0530

    feat: creates refresh token model and run alembic migrations

--- Backend/app/models/__init__.py
+++ Backend/app/models/__init__.py
@@ -2,5 +2,6 @@ from app.db.base_class import Base
 
 from .user import User
 from .session import Session, Message
+from .token import RefreshToken
 
-__all__ = ["Base", "User", "Session", "Message"]
+__all__ = ["Base", "User", "Session", "Message", "RefreshToken"]
